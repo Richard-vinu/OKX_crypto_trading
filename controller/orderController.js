8ca7cde3d29e05328a7809973418ffc78079fd9b
Richard-vinu <richardnova1999@gmail.com>
2022-10-16 23:21:43 +0530

    post

--- controller/orderController.js
+++ controller/orderController.js
@@ -4,22 +4,27 @@ import CryptoJS from "crypto-js";
 const createOrder = async (req,res)=>{
 
 try {
+ let body = req.body
 
     let timestamp=new Date().toISOString()
+    let SecretKey = '74F6E565D423A93AADDFCF729F434E32'
     let sign = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(timestamp + 'POST' + '/api/v5/trade/order', SecretKey))
-   
+   console.log(sign);
+
     var options = {
         'method': 'POST',
 
         'url': 'https://www.okx.com/api/v5/trade/order',
 
-        'data':req.body,
+        'data': body,
 
         'headers': {
         'X-API-KEY': 'bc541dd7-d800-4e15-a98b-d84a2183b7a2',
-        'OK-ACCESS-SIGN':timestamp,
-        'OK-ACCESS-TIMESTAMP': sign,
+
+        'OK-ACCESS-SIGN':sign,
+        'OK-ACCESS-TIMESTAMP': timestamp,
         'OK-ACCESS-PASSPHRASE':"Richard@123",
+       
         'Content-Type': 'application/json'
         }
       };
--- route.js
+++ route.js
@@ -1,10 +1,17 @@
-import axios from 'axios'
 import express from 'express'
 const route = express.Router()
+import { createOrder } from './controller/orderController.js'
+
 import {public_trades} from './controller/tradeController.js'
 
 route.get('/test-me',(req,res)=>{res.send('APi fired 🎇🎇')})
 
+//!orderApis
+route.post('/api/create-order',createOrder)
+
+
 route.get('/api/public-trades',public_trades)
+
+
 
 export default route
